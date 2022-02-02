4f201596de8affc5c4ca8472c58d81513d7df1ef
Mariana Carvalho Silva Teixeira <mariana.silva.teixeira@hotmail.com>
2022-02-02 17:58:43 -0300

    To-Do list and update To-Do list

--- Udemy1/index.js
+++ Udemy1/index.js
@@ -1,4 +1,45 @@
-const express = require('express')
-//import express
-const app = express()
-//initializing app
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get("/todos", (request, response) => {
+  fs.readFile("./store/todos.json", "utf-8", (err, data) => {
+    if (err) {
+      return response.status(500).send("Desculpe, algo deu errado!!");
+    }
+    const todos = JSON.parse(data);
+    return response.json({ todos: todos });
+  })
+})
+
+app.put("/todos/:id/complete", (request, response) => {
+  const id = request.params.id;
+
+  const findToDoById = (todos, id) => {
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === parseInt(id)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  fs.readFile("./store/todos.json", "utf-8", (err, data) => {
+    let todos = JSON.parse(data);
+    const todoIndex = findToDoById(todos, id);
+
+    if (todoIndex === -1) {
+      return response.status(404).send("Não encontrado!!");
+    }
+
+    todos[todoIndex].complete = true;
+
+    fs.writeFile('./store/todos.json', JSON.stringify(todos), ()=>{
+        return response.json({'status': 'Ok'})
+    })
+  })
+})
+
+app.listen(3000, () => {
+  console.log("aplicação rodando na porta http://localhost:3000/todos");
+});
