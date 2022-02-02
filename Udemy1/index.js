6107371988c88a8c2b2b8674fb9d94e7fcd49c6f
Mariana Carvalho Silva Teixeira <mariana.silva.teixeira@hotmail.com>
2022-02-02 16:33:03 -0300

    Understanding how node works

--- Udemy1/index.js
+++ Udemy1/index.js
@@ -2,3 +2,12 @@ const express = require('express')
 //import express
 const app = express()
 //initializing app
+
+
+app.get('/', (request, response)=> {
+    return response.send('Hello world!!')
+}) // executa o evento
+
+app.listen(3000, ()=>{
+    console.log('aplicação rodando na porta http://localhost:3000')
+}) //escuta o evento
